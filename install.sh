84c88547804bc37d88af712271e7ab95660e396d
jf <kubofisla@gmail.com>
2021-05-04 23:57:32 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 I3_DIR=i3
-dnf install -y i3 rofi fontawesome-fonts flatpak
+sudo dnf install -y i3 rofi fontawesome-fonts flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # Uncomment to install zoom 
 #sudo flatpak install flathub us.zoom.Zoom
