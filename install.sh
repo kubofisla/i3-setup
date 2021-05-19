a94b6f0a51ba35950ac902c691adbd592712af84
Jakub Fisla <jfisla@615GFB3-jfisla.corp.netsuite.com>
2021-05-19 09:29:37 +0200

    added dunst config, added display autoswitching app

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 I3_DIR=i3
-sudo dnf install -y i3 rofi fontawesome-fonts flatpak
+
+# Install i3 dependecies
+sudo dnf install -y i3 rofi fontawesome-fonts flatpak ImageMagick light
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # Uncomment to install zoom 
 #sudo flatpak install flathub us.zoom.Zoom
+
+## Auto display setup
+sudo dnf install -y xrandr acpid
+## Enable service for detecting lid events
+sudo systemctl enable acpid.service
+sudo systemctl start acpid.service
 
 cp $I3_DIR/config $HOME/.config/i3/
